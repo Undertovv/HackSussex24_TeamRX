5d4018b08872cb1043ef1caffc1f3fef7296cd17
undertovv <em.welter@protonmail.com>
2024-02-17 14:56:18 +0000

    Added dorito to "Misc" creative tab

--- src/main/java/rxware/RXWare.java
+++ src/main/java/rxware/RXWare.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.*;
 import org.apache.logging.log4j.LogManager;
@@ -16,7 +17,7 @@ public class RXWare implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LogManager.getLogger("rxware");
-	public static final Item rotor = new Item(new FabricItemSettings());
+	public static final Item rotor = new Item(new FabricItemSettings().group(ItemGroup.MISC));
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
