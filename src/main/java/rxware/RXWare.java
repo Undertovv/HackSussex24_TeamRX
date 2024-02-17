87768664ebd94d48daaa5fc88c2b7b2f7572cb9a
undertovv <em.welter@protonmail.com>
2024-02-17 14:29:55 +0000

    Added test item "dorito"

--- src/main/java/rxware/RXWare.java
+++ src/main/java/rxware/RXWare.java
@@ -2,20 +2,28 @@ package rxware;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import static net.minecraft.util.registry.Registry.*;
 
 public class RXWare implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LogManager.getLogger("rxware");
+	public static final Item rotor = new Item(new FabricItemSettings());
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		Registry.register(ITEM, new Identifier("rxware", "dorito"), rotor);
+		LOGGER.info("Wankel power!");
 	}
 }
