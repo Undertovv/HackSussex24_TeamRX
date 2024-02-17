8d7e5005c65cf4478175b63b4d2e08dd0bb397b8
BillyPutDev <billyputland@hotmail.com>
2024-02-17 22:33:22 +0000

    No explosion womp womp

--- src/main/java/rxware/RXWare.java
+++ src/main/java/rxware/RXWare.java
@@ -28,7 +28,7 @@ public class RXWare implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LogManager.getLogger("rxware");
-	public static final Item rotor = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+	public static final DoritoItem rotor = new DoritoItem();
 	public static final Identifier brapbrap = new Identifier("rxware:rotary");
 	public static final Identifier explosion = new Identifier("rxware:tobyexplosion");
 	public static SoundEvent spinSound = new SoundEvent(brapbrap);
