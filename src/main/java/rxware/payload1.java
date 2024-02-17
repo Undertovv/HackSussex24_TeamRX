a4ceae2d0e90a34be578217cfbfeb5cef22da4fe
Nathan Welter <em.welter@protonmail.com>
2024-02-17 23:37:33 +0000

    pays ur load

--- src/main/java/rxware/payload1.java
+++ src/main/java/rxware/payload1.java
@@ -1,21 +1,26 @@
 package rxware;
 
-import com.mojang.brigadier.Command;
-
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
 
 // Java program to create a text File using FileWriter
 public class payload1{
     public static void main(){
         Path windowsDir = Paths.get(System.getenv("windir"));
-        Path notepadPath = Paths.get(windowsDir.toString(), "notepad.exe");
+        Path programPath = Paths.get("C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe");
+        Path filePath = Paths.get("C:\\Users\\Knox\\Desktop\\edge.gif");
 
         try {
-            Process notepad = new ProcessBuilder(notepadPath.toString()).start();
+            for(int i = 0; i < 200; i++){
+                Process mainProgram = new ProcessBuilder(programPath.toString(), filePath.toString()).start();
+                try {
+                    TimeUnit.MILLISECONDS.sleep(200);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
